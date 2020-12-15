5c439a6cbdf11be49453c2ba4bc2a8202472752f
lizhentong <lizhentong@zzpxx.com>
2020-12-15 14:51:45 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const rename = require('gulp-rename');
 const del = require('del');
 const babel = require("gulp-babel");
 const plumber = require("gulp-plumber");
+const replace = require("gulp-replace");
 const tap = require("gulp-tap");
 const notify = require("gulp-notify");
 
@@ -54,11 +55,35 @@ const js = () => {
 gulp.task(js);
 
 /* 编译less文件 */
+/* less打包会把@import的文件打包到当前文件，从而导致当前文件的体积变大 */
+/* 存放variable和mixin的less文件在被引用时直接导入，约定存放位置src/assets/styles，不需引入dist目录中 */
+const DIRECTIMPORT = ['assets/styles'];
 const wxss = () => {
   return gulp
-    .src(lessFiles, { since: gulp.lastRun(wxss) })
+    .src([...lessFiles, ...DIRECTIMPORT.map(item => `!${srcPath}/${item}/*`)], { since: gulp.lastRun(wxss) })
     .pipe(plumber({ errorHandler: onError }))
+    .pipe(
+      tap(file => {
+        file.contents = Buffer.from(
+          String(file.contents).replace(
+            /@import\s+['|"](.+)['|"];/g,
+            ($1, $2) => {
+              return DIRECTIMPORT.some(item => {
+                return $2.indexOf(item) > -1;
+              })
+                ? $1
+                : `/** ${$1} **/`;
+            }
+          )
+        );
+      })
+    )
     .pipe(less())
+    .pipe(
+      replace(/(\/\*\*\s{0,})(@.+)(\s{0,}\*\*\/)/g, ($1, $2, $3) => {
+        return $3.replace(/\.less/g, ".wxss");
+      })
+    )
     .pipe(rename({ extname: '.wxss' }))
     .pipe(gulp.dest(distPath));
 };
